80e5fa3d98e52b075b111005810c3fd119859063
Ivana Vasilj <ivasilj@gmail.com>
2011-12-29 12:36:22 +0100

    show last played if no morning tune

--- morning.js
+++ morning.js
@@ -23,27 +23,25 @@ $(function(){
       var url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&format=json&limit=200&user=' + user + '&api_key=' + api_key + '&from=' + fromUTS + '&to=' + toUTS + '&callback=?';
 
       $.getJSON(url, function(data) {
-
         if (data.error) $('form').after('<p>' + data.message + '</p>');
 
         else if (data.recenttracks) {
-          if (data.recenttracks.track) {
-            var tracks = data.recenttracks.track;
-            var first = tracks.length ? tracks[tracks.length - 1] : tracks; // array or one track?
+          if (data.recenttracks.track) showTrack(data.recenttracks.track, 'form');
 
-            var tune = first.name;
-            var artist = first.artist['#text'];
-            var album = first.album['#text'];
-            var imgurl = first.image[first.image.length - 1]['#text'];
-            var time = new Date(first.date.uts * 1000);
+          else {
+            $('form').after("<p>This user's morning was without a song.</p>");
 
-            $('form').after('<section><h1>' + artist + ' - ' + tune + '</h1></section>');
-            if (imgurl)
-              $('section').append('<figure><img src="' + imgurl + '" /><figcaption> from <em>' + album + '</em></figcaption></figure>');
-            $('section').append('<p>played at <time datetime="' + time.toISOString() + '">' + time.toTimeString() + '</time></p>');
-            $('section').after('<footer><p>Powered by AudioScrobbler</p><a href="http://www.last.fm"><img alt="last.fm logo" src="lastfm_black.gif"></a></footer>');
+            // get last played instead
+            url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&format=json&limit=1&user=' + user + '&api_key=' + api_key + '&callback=?';
+
+            $.getJSON(url, function(data) {
+              if (data.recenttracks.track) {
+                $('form ~ p').filter(':last').after("<p id='last'>Last played:</p>")
+                showTrack(data.recenttracks.track, '#last');
+              }
+            });
           }
-          else $('form').after("<p>This user's morning was without a song.</p>");
+
         }
 
         else $('form').after("<p>There was a problem with fetching user's morning tune</p>");
@@ -52,5 +50,23 @@ $(function(){
 
     return false;
   });
+
+
+  showTrack = function(tracks, selector) {
+    var first = tracks.length ? tracks[tracks.length - 1] : tracks; // array or one track?
+
+    var tune = first.name;
+    var artist = first.artist['#text'];
+    var album = first.album['#text'];
+    var imgurl = first.image[first.image.length - 1]['#text'];
+    var time = new Date(first.date.uts * 1000);
+
+    $(selector).after('<section><h1>' + artist + ' - ' + tune + '</h1></section>');
+    if (imgurl)
+      $('section').append('<figure><img src="' + imgurl + '" /><figcaption> from <em>' + album + '</em></figcaption></figure>');
+    $('section').append('<p>played at <time datetime="' + time.toISOString() + '">' + time.toTimeString() + '</time></p>');
+    $('section').after('<footer><p>Powered by AudioScrobbler</p><a href="http://www.last.fm"><img alt="last.fm logo" src="lastfm_black.gif"></a></footer>');
+
+  }
 
 });
